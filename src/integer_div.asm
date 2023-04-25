1bf2f739454b3558200744a82c377a4bb9684cf5
PatriotRossii <patriotrossii2019@mail.ru>
2023-04-25 16:45:17 +0300

    Add comments in assembly code

--- src/integer_div.asm
+++ src/integer_div.asm
@@ -1,31 +1,41 @@
 global IntegerDiv_
 
 IntegerDiv_:
+    ; Arguments 1-6 are passed via registers RDI, RSI, RDX, RCX, R8, R9 respectively
+    ; Arguments 7 and above are pushed on to the stack.
+
     mov     rax, rcx
     mov     rcx, [rsp + 32]
     mov     r10, [rsp + 24]
 
     ; умножение
-    imul    edi, esi
-    imul    eax, edx
-    imul    eax, edi
+    ; (a * b * c * d)
+    imul    edi, esi ; a * b
+    imul    eax, edx ; c * d
+    imul    eax, edi ; a * b * c * d
 
     ; сложение, проверка, что не равно нулю
-    add     r8d, r9d
-    add     r8d, [rsp + 8]
-    add     r8d, [rsp + 16]
+    ; (e + f + g + h)
+    add     r8d, r9d ; e + f
+    add     r8d, [rsp + 8] ; e + f + g
+    add     r8d, [rsp + 16] ; e + f + g + h
     jz error
 
-    ; деление
+    ; знаковое расширение eax
     cdq
+    ; деление eax на r8d
     idiv    r8d
+    ; в результате в eax хранится целая часть
+    ; а в edx хранится остаток
 
-    ; расширение и запись по указателю
+    ; расширение eax в 64 бит
     cdqe
+    ; запись по указателю
     mov     [r10], rax
 
-    ; расширение и запись по указателю
+    ; расширение edx в 64 бит, перемещение в rax 
     movsxd  rax, edx
+    ; запись по указателю
     mov     [rcx], rax
 
     ; успешный код возврата
