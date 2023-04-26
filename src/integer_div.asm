f7dac3e6941a1a962c00aac065eb33f2e4b246f1
PatriotRossii <patriotrossii2019@mail.ru>
2023-04-26 13:38:01 +0300

    Simplify code using only 64-bit registers

--- src/integer_div.asm
+++ src/integer_div.asm
@@ -10,33 +10,28 @@ IntegerDiv_:
 
     ; умножение
     ; (a * b * c * d)
-    imul    edi, esi ; a * b
-    imul    eax, edx ; c * d
-    imul    eax, edi ; a * b * c * d
+    imul    rdi, rsi ; a * b
+    imul    rax, rdx ; c * d
+    imul    rax, rdi ; a * b * c * d
 
     ; сложение, проверка, что не равно нулю
     ; (e + f + g + h)
-    add     r8d, r9d ; e + f
-    add     r8d, [rsp + 8] ; e + f + g
-    add     r8d, [rsp + 16] ; e + f + g + h
+    add     r8, r9 ; e + f
+    add     r8, [rsp + 8] ; e + f + g
+    add     r8, [rsp + 16] ; e + f + g + h
     jz error
 
-    ; знаковое расширение eax
+    ; знаковое расширение
     cdq
-    ; деление eax на r8d
-    idiv    r8d
-    ; в результате в eax хранится целая часть
-    ; а в edx хранится остаток
+    ; деление rax на r8
+    idiv    r8
+    ; в результате в rax хранится целая часть
+    ; а в rdx хранится остаток
 
-    ; расширение eax в 64 бит, перемещение в rax
-    movsxd rax, eax
-    ; запись по указателю
+    ; запись целой части по указателю
     mov     [r10], rax
-
-    ; расширение edx в 64 бит, перемещение в rax 
-    movsxd  rax, edx
-    ; запись по указателю
-    mov     [rcx], rax
+    ; запись остатка по указателю
+    mov     [rcx], rdx
 
     ; успешный код возврата
     xor     eax, eax
